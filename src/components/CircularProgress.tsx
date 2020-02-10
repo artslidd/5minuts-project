c676353caa84fee9d8100d91a3d52d31de255300
artslidd <arbelleville@gmail.com>
2020-02-10 23:32:10 +0100

    Ended circular progress : https://www.youtube.com/watch?v=UMvw20nRZls&list=PLkOyNuxGl9jylDMSrg6a83bJxvNMfhoBK&index=9

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -1,37 +1,59 @@
 import * as React from 'react';
 import Animated from 'react-native-reanimated';
 import { Dimensions } from 'react-native';
-import { Svg, Circle } from "react-native-svg";
+import { Svg, Defs, LinearGradient, Stop, Path } from "react-native-svg";
 
 const { Value, interpolate, multiply } = Animated;
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const { PI } = Math;
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 const { width } = Dimensions.get("window");
 const size = width - 32;
 const strokeWidth = 50;
 const radius = (size - strokeWidth) / 2;
-const circumference = radius * 2 * Math.PI;
+const cx = size / 2;
+const cy = size / 2;
+const A = PI + PI * 0.4;
+const startAngle = PI + PI * 0.2
+const endAngle = 2 * PI - PI * 0.2
+
+const startX = cx - radius * Math.cos(startAngle);
+const startY = cy - radius * Math.sin(startAngle);
+const endX = cx - radius * Math.cos(endAngle);
+const endY = cx - radius * Math.sin(endAngle);
+
+const d = `M ${startX} ${startY} A ${radius} ${radius} 0 1 0 ${endX} ${endY}`
 
 interface CircularProgressProps {
     progress: typeof Value
 };
 
 export default ({ progress }: CircularProgressProps) => {
+    const length = radius * A;
     const alpha = interpolate(progress, {
         inputRange: [0, 1],
-        outputRange: [0, 2 * Math.PI]
+        outputRange: [0, A]
     });
 
     const strokeDashoffset = multiply(alpha, radius);
 
     return <Svg width={size} height={size}>
-        <AnimatedCircle
+        <Defs>
+            <LinearGradient id='grad' x1='0' y1='0' x2='100%' y2='0'>
+                <Stop offset="0" stopColor="#f7cd46"></Stop>
+                <Stop offset="1" stopColor="#ef9837"></Stop>
+            </LinearGradient>
+        </Defs>
+        <Path
             fill="none"
-            stroke="#2162cc"
-            cx={size / 2}
-            cy={size / 2}
-            r={radius}
-            strokeDasharray={`${circumference} ${circumference}`}
-            {...{ strokeWidth, strokeDashoffset }}
+            stroke="white"
+            strokeDasharray={`${length} ${length}`}
+            {...{ strokeWidth, d }}
+        />
+        <AnimatedPath
+            fill="none"
+            stroke="url(#grad)"
+            strokeDasharray={`${length} ${length}`}
+            {...{ strokeWidth, strokeDashoffset, d }}
         />
     </Svg>
 };
